2207f0a5785b423f4b70b2e8276f90584b9fe45f
Marchuk Dmytro <marcukdyma@gmail.com>
2024-09-26 12:43:39 +0200

    fixed styles of the links

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -58,13 +58,16 @@ function About(): JSX.Element {
                <li className={styles.listElement_withIcons}>
                   <svg
                      xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 512 512"
-                     width="20"
+                     width="24"
+                     height="24"
+                     viewBox="0 0 24 24"
+                     fill="none"
+                     stroke="#e0342e"
+                     strokeWidth="2"
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
                   >
-                     <path
-                        fill="#e0342e"
-                        d="M439.62 396.531a3153555.92 3153555.92 0 0 1-54.398-65.832c-1.45-1.755-3.067-3.614-5.289-4.107-2.903-.643-5.7 1.25-8.073 3.043l-40.219 30.384c-8.03 6.067-17.968 12.468-27.433 9.051-5.483-1.98-9.312-6.857-12.834-11.504-20.403-26.911-40.806-53.824-61.211-80.735l-.372-.507-58.748-82.546c-3.381-4.75-6.89-9.86-7.141-15.684-.432-10.055 8.647-17.625 16.836-23.475 13.673-9.765 27.345-19.532 41.017-29.298 2.42-1.728 5.064-3.832 5.317-6.794.195-2.268-1.097-4.367-2.338-6.274l-46.561-71.589c-5.304-8.154-11.224-16.814-20.362-20.15-10.658-3.889-22.367.601-32.784 5.096-17.341 7.483-35.259 15.614-47.316 30.152-20.164 24.317-19.45 60.744-17.678 90.53 2.075 34.883 13.587 70.892 27.119 102.854 13.997 33.06 32.551 64.087 54.536 92.485 20.489 29.369 44.486 56.266 71.773 79.438 26.456 22.467 57.378 44.216 90.045 56.624 27.893 10.595 62.442 22.164 91.674 10.19 17.477-7.16 30.593-21.828 42.918-36.139 7.403-8.597 15.188-18.429 14.661-29.763-.448-9.713-6.943-17.952-13.139-25.45z"
-                     ></path>
+                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                   </svg>
                   <span>+02131 2312 3123</span>
                </li>
@@ -72,13 +75,17 @@ function About(): JSX.Element {
                <li className={styles.listElement_withIcons}>
                   <svg
                      xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 -5 32 32"
-                     width="20"
+                     width="24"
+                     height="24"
+                     viewBox="0 0 24 24"
+                     fill="none"
+                     stroke="#e0342e"
+                     strokeWidth="2"
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
                   >
-                     <path
-                        fill="#e0342e"
-                        d="M27 4.71H5a4.505 4.505 0 0 0-4.5 4.5v13.58c0 2.481 2.019 4.5 4.5 4.5h22c2.481 0 4.5-2.019 4.5-4.5V9.21c0-2.481-2.019-4.5-4.5-4.5zm-1.33 3L24.29 8.8l-7.36 5.81c-.55.43-1.31.43-1.86 0L7.97 9 6.333 7.71h19.338zM27 24.29H5c-.827 0-1.5-.673-1.5-1.5V9.305l9.71 7.665c.82.65 1.81.97 2.79.97s1.97-.32 2.79-.97l9.71-7.662V22.79c0 .827-.673 1.5-1.5 1.5z"
-                     ></path>
+                     <rect width="20" height="16" x="2" y="4" rx="2" />
+                     <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
                   </svg>
                   <span>my_email@email.com</span>
                </li>
@@ -88,21 +95,70 @@ function About(): JSX.Element {
             <p>
                During the development, we used such technologies as{" "}
                <strong>Vite</strong>, <strong>React</strong>,{" "}
-               <strong>Github</strong> and <strong>SSS</strong>. The images for
-               the examples set of flash cards were taken from the next
-               resources: <strong>DODO Pizza</strong>. Figma was used to draw
-               the icons. Also here you can see the social media of the author:
+               <strong>Github</strong> and <strong>CSS</strong>. The images for
+               pizzas were taken from the{" "}
+               <a href="https://dodopizza.de/" target="_blank">
+                  DODO PIZZA site
+               </a>
+               . Figma was used to draw the icons. Also here you can see the
+               social media of the author:
             </p>
             <ul className={styles.listConteiner}>
-               <li className={styles.listElement}>
-                  <a href="https://github.com/Gafum">GitHub</a>
+               <li className={styles.listElement_withIcons}>
+                  <a href="https://github.com/Gafum">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="#e0342e"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                     >
+                        <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                        <path d="M9 18c-4.51 2-5-2-7-2" />
+                     </svg>
+
+                     <span>GitHub</span>
+                  </a>
                </li>
-               <li className={styles.listElement}>
-                  <a href="https://www.youtube.com/@gafum">YouTube</a>
+               <li className={styles.listElement_withIcons}>
+                  <a href="https://www.youtube.com/@gafum">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="#e0342e"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                     >
+                        <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" />
+                        <path d="m10 15 5-3-5-3z" />
+                     </svg>
+                     <span>YouTube</span>
+                  </a>
                </li>
-               <li className={styles.listElement}>
+               <li className={styles.listElement_withIcons}>
                   <a href="https://play.google.com/store/apps/dev?id=5298640550031789087&gl=DE">
-                     Google Play
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="#e0342e"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                     >
+                        <polygon points="6 3 20 12 6 21 6 3" />
+                     </svg>
+                     <span>Google Play</span>
                   </a>
                </li>
             </ul>
